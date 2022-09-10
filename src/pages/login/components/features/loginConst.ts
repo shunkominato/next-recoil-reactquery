5ab9a0f59a3bf973fefc7a7b7a059bb3f2bddb5a
shunkominato <shun19871001@gmail.com>
2022-09-10 09:23:24 +0900

    useQuery

--- src/pages/login/components/features/loginConst.ts
+++
@@ -1,1 +0,0 @@
-export const LOGIN_URL = `${process.env.NEXT_PUBLIC_API_URL as string}`;
--- src/pages/todo/components/Todo.tsx
+++ src/pages/todo/components/Todo.tsx
@@ -3,12 +3,15 @@ import { FC } from 'react';
 import { useForm } from 'react-hook-form';
 // import { useAddTodo } from './useAddTodo';
 import { todoState } from './features/todoAtom';
+import { useQueryTasks } from './features/useQueryTasks';
 
 type FormValue = {
   todo: string;
 };
 
 export const Todo: FC = () => {
+  const { data: tasks, status } = useQueryTasks()
+  console.log(tasks?.length);
   const [todo, setText] = useRecoilState(todoState);
   // const { addTodo } = useAddTodo();
   const { register, handleSubmit } = useForm<FormValue>();
@@ -23,7 +26,7 @@ export const Todo: FC = () => {
         <input {...register('todo')} />
         <input type="submit" />
       </form>
-      <span>{todo}</span>
+      <span>{tasks?.map((task => <span key={task.id}>{task.title}</span>))}</span>
     </>
   );
 };
