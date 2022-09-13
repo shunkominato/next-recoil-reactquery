5dbd48d10f7d2d4eab2fcb077df71333ed8e8bb0
shunkominato <shun19871001@gmail.com>
2022-09-13 21:59:44 +0900

    latest next

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,21 @@
 import { FC, ReactNode } from 'react'
 import Head from 'next/head'
+import { createStyles, Button, Menu, Group, ActionIcon } from '@mantine/core';
+// import { Button } from '@/components/button/Button'
 
 type Props = {
   title: string
   children: ReactNode
 }
 
-export const Layout: FC<Props> = ({ children, title = 'Nextjs' }) => {
+export const Layout: FC<Props> = ({ children, title = 'FAクラフト' }) => {
   return (
-    <div className="">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
         <title>{title}</title>
       </Head>
       <main className="flex w-screen flex-1 flex-col items-center justify-center">
+      <Button>送信</Button>
         {children}
       </main>
     </div>
