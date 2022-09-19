ef0970d61ce2c38347ff72820fe2cb2d1aed5a40
shunkominato <shun19871001@gmail.com>
2022-09-19 18:41:40 +0900

    zod add

--- src/pages/login/components/features/validation.ts
+++ src/pages/login/components/features/validation.ts
@@ -1,9 +1,9 @@
-import * as yup from 'yup';
+import { z } from 'zod';
 import { LoginFormTypes } from '../Login';
 
-export const validationSchema = yup.object({
-  email: yup.string().required('入力必須項目です。').email('入力されたメールアドレスに間違いがあります。'),
-  password: yup.string().required('入力必須項目です。'),
+export const validationSchema = z.object({
+  email: z.string().min(1).email('入力されたメールアドレスに間違いがあります。'),
+  password: z.string().min(1),
 });
 
-export type Form = yup.InferType<typeof validationSchema>;
+export type Form = z.infer<typeof validationSchema>;
