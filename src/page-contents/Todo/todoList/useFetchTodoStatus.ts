3f9a39b8a39d07ff18194227c1b35152d5e643ff
shunkominato <shun19871001@gmail.com>
2023-01-05 08:40:03 +0900

    _.

--- src/page-contents/Todo/todoList/useFetchTodoStatus.ts
+++ src/page-contents/Todo/todoList/useFetchTodoStatus.ts
@@ -2,28 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import router from 'next/router';
 import { AxiosError } from 'axios';
 import { todoStatusApi } from './todoApi';
+import { errorHandler } from '@/util/errorHandler';
 
 export const useFetchTodoStatus = () => {
   const { data, isLoading, isError } = useQuery({
     queryKey: ['todoStatus'],
     queryFn: todoStatusApi,
     onError: (err: AxiosError) => {
-      if (err.response?.status === 401 || err.response?.status === 403) {
-        window.alert(
-          'ログイン状態が無効になりました。続けて操作する場合はログインをしなおしてください。'
-        );
-        router.push('/sign_up');
-        return;
-      }
-      if (err.message === 'Network Error') {
-        window.alert(
-          '通信がエラー発生しました。電波の良い場所で再度お試しください'
-        );
-        return;
-      }
-      window.alert(
-        '予期せぬエラーが発生しました。しばらくしてから再度お試しください'
-      );
+      errorHandler({ err, alertMessage: '失敗しました' });
     },
   });
 
---
+++ src/util/errorHandler.ts
@@ -0,0 +1,31 @@
+import { AxiosError } from 'axios';
+import router from 'next/router';
+
+export const errorHandler = ({
+  err,
+  alertMessage,
+}: {
+  err: AxiosError;
+  alertMessage?: string;
+}) => {
+  if (err.response?.status === 401 || err.response?.status === 403) {
+    window.alert(
+      'ログイン状態が無効になりました。続けて操作する場合はログインをしなおしてください。'
+    );
+    router.push('/sign_up');
+    return;
+  }
+
+  if (err.message === 'Network Error') {
+    window.alert(
+      '通信がエラー発生しました。電波の良い場所で再度お試しください'
+    );
+    return;
+  }
+
+  window.alert(
+    alertMessage
+      ? alertMessage
+      : '予期せぬエラーが発生しました。しばらくしてから再度お試しください'
+  );
+};
